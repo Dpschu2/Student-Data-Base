cc5d001f8175d1e6c2c3aa42ff258c976a5ff471
Schulz, Dean <dpschu2@ilstu.edu>
2017-10-03 15:50:00 -0500

    new driver

--- GradeBookDriver.java
+++ GradeBookDriver.java
@@ -1,6 +1,41 @@
-class GradeBookDriver {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-  public static void main(String[] args) {
-    
-  }
+public class GradeBookDriver {
+
+	public static void main(String[] args) {
+		String csvFile = "226-fall-1996.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+       
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+	}
+
 }
