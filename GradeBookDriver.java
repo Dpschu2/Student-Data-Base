596484fc025f74d0399d9fab07d7540b34e7ef3a
Schulz, Dean <dpschu2@ilstu.edu>
2017-10-04 01:04:02 -0500

    files are put into an array

--- GradeBookDriver.java
+++ GradeBookDriver.java
@@ -2,37 +2,60 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GradeBookDriver {
 
+	public static ArrayList<ArrayList<String>> mainArray = new ArrayList<>();
+	public static ArrayList<String> layer1 = new ArrayList<>();
+	public static ArrayList<String> layer2 = new ArrayList<>();
+	public static ArrayList<String> layer3 = new ArrayList<>();
+	
 	public static void main(String[] args) {
+		setUp("226-fall-1996.csv");
+		setUp("326-fall-1996.csv");
+		setUp("326-fall-1997.csv");
+		mainArray.add(layer1);
+		mainArray.add(layer2);
+		mainArray.add(layer3);
         Scanner kb = new Scanner(System.in);
-        displayMenu();
-        String choice = kb.nextLine();
-
-        switch (choice.substring(0,1).toLowerCase()){
-            case "a":
-                addData();
-                break;
-            case "s":
-                saveData();
-                break;
-            case "g":
-                studentsPerGrade();
-                break;
-            case "e":
-                System.exit(0);
-                break;
-        }
+        boolean flag = false;
+        do {
+	        displayMenu();
+	        String choice = kb.nextLine();
+	        switch (choice.substring(0,1).toLowerCase()){
+	            case "a":
+	                addData();
+	                flag = false;
+	                break;
+	            case "s":
+	                saveData();
+	                flag = false;
+	                break;
+	            case "g":
+	                studentsPerGrade();
+	                flag = false;
+	                break;
+	            case "e":
+	                System.exit(0);
+	                flag = false;
+	                break;
+	            default:
+	            	System.out.println("Invalid option.\n");
+	            	flag = true;
+	        }
+        } while(flag);
+        flag = false;
+        kb.close();
     }
-    public void addData(){
+    public static void addData(){
 
     }
-    public void saveData(){
+    public static void saveData(){
 
     }
-    public void studentsPerGrade(){
+    public static void studentsPerGrade(){
 
     }
     public void exit(){
@@ -48,22 +71,29 @@ public class GradeBookDriver {
     }
 
 
-	public static void readFile(){
-        String csvFile = "226-fall-1996.csv";
+	public static void setUp(String s){
+        String csvFile = s;
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
 
         try {
-
-            br = new BufferedReader(new FileReader(csvFile));
-            //read file line by line until the end
+            br = new BufferedReader(new FileReader(csvFile)); 
             while ((line = br.readLine()) != null) {
 
-                String[] country = line.split(cvsSplitBy);
-
-                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
-
+                String[] categories = line.split(cvsSplitBy);
+                for(int i =  0; i < categories.length; i++){
+                	if(s.equals("226-fall-1996.csv")) {
+                		layer1.add(categories[i]);
+                	}
+                	if(s.equals("326-fall-1996.csv")) {
+                		layer2.add(categories[i]);
+                	}
+                	if(s.equals("326-fall-1997.csv")) {
+                		layer3.add(categories[i]);
+                	}
+              
+               }
             }
 
         } catch (FileNotFoundException e) {
@@ -79,6 +109,7 @@ public class GradeBookDriver {
                 }
             }
         }
+        
     }
 
 }
